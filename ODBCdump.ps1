c1faf18db2d8f6a9d70660e99c752269b91a8445
LorenzoGodi <lorenzo.godi@outlook.it>
2023-12-15 11:17:24 +0100

    Create ODBCdump.ps1

---
+++ ODBCdump.ps1
@@ -0,0 +1,41 @@
+﻿$outputFolder = "C:\"
+
+#"Driver","APILevel","FileUsage,"Setup","ConnectFunctions","CPTimeout","DriverODBCVer","SQLLevel","HelpRootDirectory"
+$keys = @("Driver","APILevel","FileUsage","Setup","ConnectFunctions","CPTimeout","DriverODBCVer","SQLLevel","HelpRootDirectory")
+
+$drivers = Get-OdbcDriver | Sort-Object -Property Name
+
+# Columns
+$names = New-Object System.Collections.Generic.List[String]
+$columns = New-Object System.Collections.Generic.List[PSObject]
+
+$names.Add("Name")
+$names.Add("Platform")
+
+$columns.Add(($drivers | ForEach-Object {$_.Name}))
+$columns.Add(($drivers | ForEach-Object {$_.Platform}))
+
+foreach ($key in $keys){
+    $names.Add($key)
+    $columns.Add(($drivers | ForEach-Object {$_.Attribute[$key]}))
+}
+
+# Get data
+$rows = New-Object System.Collections.Generic.List[PSObject]
+
+for ($i = 0; $i -lt $drivers.Length; $i++){
+    $row = New-Object PSObject
+
+    $j = 0
+    foreach ($col in $columns){
+        $row | Add-Member -NotePropertyName $names[$j++] -NotePropertyValue $col[$i]
+    }
+
+    $rows.Add($row)
+}
+
+# Export
+$rows | Export-Csv -Path ($outputFolder + "ODBC.csv") -NoTypeInformation
+
+$rows | Where-Object {$_.Platform -eq "32-bit"} | Export-Csv -Path ($outputFolder + "ODBC_32.csv") -NoTypeInformation
+$rows | Where-Object {$_.Platform -eq "64-bit"} | Export-Csv -Path ($outputFolder + "ODBC_64.csv") -NoTypeInformation
