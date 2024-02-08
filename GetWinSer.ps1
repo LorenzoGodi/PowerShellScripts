6c6f79b68268a51c00e4ede27e98e541d2ae71dc
LorenzoGodi <lorenzo.godi@outlook.it>
2024-02-08 14:34:42 +0100

    Create GetWinSer.ps1

---
+++ GetWinSer.ps1
@@ -0,0 +1,25 @@
+﻿$OutputDir = 'C:\'
+
+
+Function Get-IP ($hn) {
+    try {
+        ([System.Net.Dns]::GetHostAddresses($hn)).IPAddressToString
+    }
+    catch {
+        'X'
+    }
+}
+
+
+$WindowsServers = Get-ADComputer -Filter 'OperatingSystem -like "*windows server*"'
+
+
+$tab = @()
+for ($i = 0; $i -lt $WindowsServers.Length; $i++){
+    $tab += [PSCustomObject]@{
+        hostname = $WindowsServers[$i].name
+        ipadd = Get-IP($WindowsServers[$i].name)
+    }
+    Write-Progress -activity "Getting all servers..." -status "Completed: $i of $($WindowsServers.Length)" -percentComplete (($i / $WindowsServers.Length)  * 100)
+}
+$tab | Export-Csv -Path ($OutputDir + 'GetWinSer.csv') -NoTypeInformation
