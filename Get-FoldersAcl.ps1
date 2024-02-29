c717476fc3a685e45163dd3ad716a391aa5a6987
LorenzoGodi <34336808+LorenzoGodi@users.noreply.github.com>
2024-02-29 18:02:51 +0100

    Update Get-FoldersAcl.ps1

--- Get-FoldersAcl.ps1
+++ Get-FoldersAcl.ps1
@@ -1,19 +1,58 @@
-﻿function Get-FoldersAcl {
+﻿function Test-PathActive {
+    param (
+        [Parameter(Mandatory=$true)]
+        $Path
+    )
 
+    $script = "Test-Path -Path " + $Path
+    $ps = [powershell]::create().addscript($script)
+    
+    # execute it asynchronously
+    $handle = $ps.begininvoke()
+    
+    # wait 4 seconds for it to finish
+    if(-not $handle.asyncwaithandle.waitone(4000)){
+        return $false
+    }
+    
+    # waitone() returned $true, let's fetch the result
+    $result = $ps.endinvoke($handle)
+    
+    return $result
+}
+
+function Get-FoldersAcl {
     param (
         [Parameter(Mandatory=$true)]
         $MainPath,
         [Parameter(Mandatory=$true)]
-        $IsDFS = $false,
+        $IsDFS,
+        [Parameter(Mandatory=$true)]
+        $Levels
+    )
+
+    $ScpProgressActivity = "Collecting all folders. Arm yourself with patience..."
+    $scpProgressIndex = -1
+    $scpProgressTotal = -1
+    $scpProgressPercent = 0
+    $scpProgressScan = ""
+
+    return Get-FoldersAclRec -MainPath $MainPath -IsDFS $IsDFS -Levels $Levels -ReturnCommons $true -Progress -1
+}
+
+function Get-FoldersAclRec {
+    param (
+        [Parameter(Mandatory=$true)]
+        $MainPath,
+        [Parameter(Mandatory=$true)]
+        $IsDFS,
         [Parameter(Mandatory=$true)]
         $Levels,
-        [Parameter(Mandatory=$false)]
-        $IgnoreLinkFolders = $true,
-        [Parameter(Mandatory=$false)]
-        $ReturnCommons = $true,
-        [Parameter(Mandatory=$false)]
-        $ShowProgress = $true
+        [Parameter(Mandatory=$true)]
+        $ReturnCommons
     )
+
+    $ImRoot = $scpProgressIndex -eq -1
     
     # Create list of objects representing folders
     $folders = @()
@@ -24,8 +63,8 @@
     # Get all folders excluding link folders
     $FoldersRaw = Get-ChildItem -Path $MainPath -Directory -Force -ErrorAction SilentlyContinue
 
-    # Remove link folders if not requested
-    if ($IgnoreLinkFolders) {
+    # Remove link folders if not dfs
+    if (!$IsDFS) {
         $FoldersRaw = $FoldersRaw | Where-Object {$_.Mode -notmatch "d----l"}
     }
 
@@ -44,82 +83,91 @@
             # Create folder object
             $folder = new-object PSObject
 
-            # Get path
-            $folderPath = $folderRaw.PSPath.Substring($folderRaw.PSPath.IndexOf("::") + 2)
+            # Get name and path
+            $folderName = $folderRaw.Name
+            $folderPath = $folderRaw.PSPath.Split("::")[-1]
+
+            # Update main progress values if root function
+            if ($ImRoot) {
+                $scpProgressIndex = $i + 1
+                $scpProgressTotal = $FoldersRaw.Length
+                $scpProgressPercent = ($scpProgressIndex / $scpProgressTotal)  * 100
+            }
+            
+            # Write progress
+            $strStatus = "$($scpProgressIndex - 1) of $scpProgressTotal folders scanned."
+            $strStatus += " Now scanning: $folderPath"
+            Write-Progress -activity $ScpProgressActivity -status $strStatus -percentComplete $scpProgressPercent
 
             if ($IsDFS) {
                 # Obtained path was dfs
-                $folder | Add-Member -NotePropertyName LinkName -NotePropertyValue $folderRaw.Name
+                $folder | Add-Member -NotePropertyName LinkName -NotePropertyValue $folderName
                 $folder | Add-Member -NotePropertyName LinkPath -NotePropertyValue $folderPath
 
                 # Get Target folder
-                $folderTargets = Get-DfsnFolderTarget -Path $folderPathLink | Where-Object {$_.State -eq "Online"} | ForEach-Object {$_.TargetPath} | Select -Unique
+                $folderTargets = Get-DfsnFolderTarget -Path $folderPath | Where-Object {$_.State -eq "Online"} | ForEach-Object {$_.TargetPath} | Select -Unique
 
-                # Effective name and paths
-                $folder | Add-Member -NotePropertyName Name -NotePropertyValue ($folderTargets | ForEach-Object {$_.Substring($_.IndexOf("ne$\") + 4)})[0]
-                $folder | Add-Member -NotePropertyName Paths -NotePropertyValue $folderTargets
+                # Effective paths
+                $folder | Add-Member -NotePropertyName DFSPaths -NotePropertyValue $folderTargets
 
                 # Set one effective path for getting acls
-                $folderPath = $folderTargets[0]
+                $folderPathMissing = $true
+                $i = -1
+                while ($folderPathMissing) {
+                    $folderPathMissing = -not (Test-PathActive -Path $folderTargets[++$i])
+                }
+                $folderPath = $folderTargets[$i]
+
+                # Set effective name
+                $folderName = $folderPath.Split("\")[-1]
             }
-            
+
+            # Effective name and path
+            $folder | Add-Member -NotePropertyName Name -NotePropertyValue $folderName
+            $folder | Add-Member -NotePropertyName Path -NotePropertyValue $folderPath
 
             # Get ACLs
             $folderAcl = (Get-Acl $folder.Path).Access
 
+            #
+            $subAclCommonMembers = @()
+
+            # If necessary, collect subfolders
+            if ($Levels -gt 1) {
+                # Get all subfolders
+                $subReturns = Get-FoldersAclRec -MainPath $folder.Path -IsDFS $false -Levels ($Levels - 1)
+
+                # Add
+                $folder | Add-Member -NotePropertyName Children -NotePropertyValue $subReturns.folders
+
+                # Leave in common acls only shared ones
+                $aclCommonMembers = $aclCommonMembers| Where-Object {$_.IdentityReference -in ($subReturns.CommoncAcls | Select -Property IdentityReference)}
+            }
+
+            # Filter
             if ($ReturnCommons) {
                 # Leave in common acls only shared ones
-                $aclCommonMembersNew = @()
-                foreach ($oldAcl in $aclCommonMembers) {
-                    # Tells if acl is still common
-                    $stillCommon = $false
-
-                    # If found, boolean becomes true
-                    foreach ($fldAcl in $folderAcl) {
-                        if ($fldAcl.IdentityReference -eq $oldAcl.IdentityReference) {
-                            $stillCommon = $true
-                        }
-                    }
-
-                    # If found, keep in commons
-                    $aclCommonMembersNew += $oldAcl
-                }
-                $aclCommonMembers = $aclCommonMembersNew
+                $aclCommonMembers = $aclCommonMembers | Where-Object {$_.IdentityReference -in ($folderAcl | Select -Property IdentityReference)}
             }
 
             # Add ACLs
             $folder | Add-Member -NotePropertyName Access -NotePropertyValue $folderAcl
 
-            # If necessary, collect subfolders
-            if ($Levels -gt 1) {
-                # Get all subfolders
-                $subFolders = Get-FoldersAcl -MainPath $folder.Path -Levels ($Levels - 1) -IgnoreLinkFolders $true -ReturnCommons $false -ShowProgress $false
-
-                # Add
-                $folder | Add-Member -NotePropertyName Children -NotePropertyValue $subFolders
-            }
-
             # Add object to array
             $folders += $folder
-            
-            # Write progress
-            if ($ShowProgress) {
-                Write-Progress -activity "Collecting all folders. Arm yourself with patience..." -status "Completed: $i of $($foldersRaw.Length) items scanned" -percentComplete (($i / $foldersRaw.Length)  * 100)
-            }
         }
     }
     
     # Order by name
     $folders = $folders | Sort-Object -Property Name
 
-    # Return multiple objects
-    $result = new-object PSObject
-    $result | Add-Member -NotePropertyName Folders -NotePropertyValue $folders
-    $result | Add-Member -NotePropertyName CommonAcls -NotePropertyValue $aclCommonMembers
-
-
     # Return
     if ($ReturnCommons) {
+        # Return multiple objects
+        $result = new-object PSObject
+        $result | Add-Member -NotePropertyName Folders -NotePropertyValue $folders
+        $result | Add-Member -NotePropertyName CommoncAcls -NotePropertyValue $aclCommonMembers
+
         return $result
     } else {
         return $folders
