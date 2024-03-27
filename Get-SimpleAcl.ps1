7e20ac71660d0fc9f06aa5234bf2fd8e43dbdb19
LorenzoGodi <34336808+LorenzoGodi@users.noreply.github.com>
2024-03-27 11:55:45 +0100

    Create Get-SimpleAcl.ps1

---
+++ Get-SimpleAcl.ps1
@@ -0,0 +1,27 @@
+﻿function Get-SimpleAcl {
+    param (
+        [Parameter(Mandatory=$true)]
+        $Path
+    )
+
+    $acls = (Get-Acl $Path).Access | Where-Object {$_.IsInherited -eq $false}
+    
+    $aclFull = $acls | Where-Object { $_.FileSystemRights -like 'FullControl' }
+    $aclMody = $acls | Where-Object { $_.FileSystemRights -like '*Modify*' }         | Where-Object { $_ -notin $aclFull }
+    $aclExec = $acls | Where-Object { $_.FileSystemRights -like '*ReadAndExecute*' } | Where-Object { $_ -notin $aclMody }
+    $aclTrav = $acls | Where-Object { $_.FileSystemRights -like 'FullControl' }      | Where-Object { $_ -notin $aclExec -and $_ -notin $aclMody }
+
+    $types = @('(F)  ', '(M)  ', '(RX) ', '(RD) ')
+    $arrs = @($aclFull, $aclMody, $aclExec, $aclTrav)
+
+    $result = @()
+
+    for ($i = 0; $i -lt 4; $i++) {
+        $result += ''
+        foreach ($gp in $arrs[$i]) {
+            $result += $types[$i] + $gp.IdentityReference
+        }
+    }
+
+    return $result
+}
