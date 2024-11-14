8922f70b39d73a4be14df36daea6cb3d8f38457a
LorenzoGodi <34336808+LorenzoGodi@users.noreply.github.com>
2024-11-14 12:01:28 +0100

    Update Get-NestedAcl.ps1

--- Get-NestedAcl.ps1
+++ Get-NestedAcl.ps1
@@ -1,80 +1,78 @@
-﻿function Get-SimpleAcl {
-    param (
-        [Parameter(Mandatory=$false)] $Path,
-        [Parameter(Mandatory=$false)] $Lv = 2
-    )
+﻿param (
+    [Parameter(Mandatory=$false)] $Path,
+    [Parameter(Mandatory=$false)] $Lv = 2
+)
 
-    # Get path from clipboard if present
-    $clipAd = ''
-    if (!$PSBoundParameters.ContainsKey('Path')) {
-        $clipAd = '(ClipBoard) '
-        $Path = Get-Clipboard
+# Get path from clipboard if present
+$clipAd = ''
+if (!$PSBoundParameters.ContainsKey('Path')) {
+    $clipAd = '(ClipBoard) '
+    $Path = Get-Clipboard
 
-        $clipAd += ' '
-    }
+    $clipAd += ' '
+}
 
-    # Get all acls for main directory
-    $acls = (Get-Acl $Path).Access | Sort-Object -Property IsInherited
-    
-    $aclFull = $acls | Where-Object { $_.FileSystemRights -like 'FullControl' }
-    $aclMody = $acls | Where-Object { $_.FileSystemRights -like '*Modify*' } | Where-Object { $_ -notin $aclFull }
-    $aclExec = $acls | Where-Object { $_.FileSystemRights -like '*ReadAndExecute*' } | Where-Object { $_ -notin $aclMody }
-    $aclTrav = $acls | Where-Object { $_ -notin $aclExec -and $_ -notin $aclMody  -and $_ -notin $aclFull }
+# Get all acls for main directory
+$acls = (Get-Acl $Path).Access | Sort-Object -Property IsInherited
 
-    $types = @('(F)  ', '(M)  ', '(RX) ', '(RD) ')
-    $arrs = @($aclFull, $aclMody, $aclExec, $aclTrav)
+$aclFull = $acls | Where-Object { $_.FileSystemRights -like 'FullControl' }
+$aclMody = $acls | Where-Object { $_.FileSystemRights -like '*Modify*' } | Where-Object { $_ -notin $aclFull }
+$aclExec = $acls | Where-Object { $_.FileSystemRights -like '*ReadAndExecute*' } | Where-Object { $_ -notin $aclMody }
+$aclTrav = $acls | Where-Object { $_ -notin $aclExec -and $_ -notin $aclMody  -and $_ -notin $aclFull }
 
-    # output is a simple array of strings
-    $sep = '------   ------   ------   ------   ------   ------'
-    $result = @('', $sep, "$($clipAd)ACLs for: $($Path)", $sep)
+$types = @('(F)  ', '(M)  ', '(RX) ', '(RD) ')
+$arrs = @($aclFull, $aclMody, $aclExec, $aclTrav)
+
+# output is a simple array of strings
+$sep = '------   ------   ------   ------   ------   ------'
+Write-Host ''
+Write-Host $sep
+Write-Host "$($clipAd)ACLs for: $($Path)"
+Write-Host $sep
 
 
-    # Check for folders with non-canonical acls
-    $result += ''
-    $result += 'Folders with inheritance disabled:'
-    $noSpecial = $true
+# Check for folders with non-canonical acls
+Write-Host ''
+Write-Host 'Folders with inheritance disabled:'
+$noSpecial = $true
 
-    # Get all subfolders
-    $subFolders = Get-ChildItem -Path $Path -Recurse -Depth $Lv -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
+# Get all subfolders
+$subFolders = Get-ChildItem -Path $Path -Recurse -Depth $Lv -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
 
-    # Check
-    $lvCount = 0
-    $startingSlashCount = ([regex]::Matches($Path, "\\" )).count
-    foreach ($sf in $subFolders) {
-        # Get all inherited acls for directory
-        $acls = (Get-Acl $sf.FullName).Access | Where-Object { $_.IsInherited -eq $true }
+# Check
+$lvCount = 0
+$startingSlashCount = ([regex]::Matches($Path, "\\" )).count
+foreach ($sf in $subFolders) {
+    # Get all inherited acls for directory
+    $acls = (Get-Acl $sf.FullName).Access | Where-Object { $_.IsInherited -eq $true }
 
-        if ($acls -eq $null) {
-            $slashCount = ([regex]::Matches($sf.FullName, "\\" )).count
-            if ($startingSlashCount -ne $slashCount) {
-                $lvCount += $slashCount - $startingSlashCount
-                $startingSlashCount = $slashCount
-                $result += '    ' + "[$($lvCount)]"
-            }
-            $result += '    ' + $sf.FullName
-            $noSpecial = $false
+    if ($acls -eq $null) {
+        $slashCount = ([regex]::Matches($sf.FullName, "\\" )).count
+        if ($startingSlashCount -ne $slashCount) {
+            $lvCount += $slashCount - $startingSlashCount
+            $startingSlashCount = $slashCount
+            Write-Host '    ' "[$($lvCount)]"
         }
+        Write-Host '    ' $sf.FullName
+        $noSpecial = $false
     }
+}
 
-    if ($noSpecial) {
-        $result += '    # none #'
-    }
+if ($noSpecial) {
+    Write-Host '    # none #'
+}
 
 
-    $result += ''
+Write-Host ''
 
-    # ACLs
-    for ($i = 0; $i -lt 4; $i++) {
-        $result += ''
-        foreach ($gp in $arrs[$i]) {
-            $inh = '    '
-            if ($gp.IsInherited) {
-                $inh = '[H] '
-            }
-            $result += $inh + $types[$i] + $gp.IdentityReference
+# ACLs
+for ($i = 0; $i -lt 4; $i++) {
+    Write-Host ''
+    foreach ($gp in $arrs[$i]) {
+        $inh = '    '
+        if ($gp.IsInherited) {
+            $inh = '[H] '
         }
+       Write-Host $inh $types[$i] $gp.IdentityReference
     }
-
-    # Return
-    return $result
 }
